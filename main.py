0d91348561cfaa8fccb2bd643abef71815755b52
sahilchhillar <panchalchh@outlook.com>
2023-09-11 21:46:30 +0100

    Main function changes

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from car.car import Car
-from car_factory.model.calliope import Callipe
 from datetime import date
 from engines.capulet_engine import CapuletEngine
 from battery.spindler_battery import SprindlerBattery
+from car_factory.car_factory import CarFactory
 
 if __name__ == "__main__":
     capuletEngine = CapuletEngine(current_mileage=60000, last_service_mileage=20000)
     sprindlerBattery = SprindlerBattery(last_service_date=date.today(), current_date=date.today())
 
     car = Car(engine=capuletEngine, battery=sprindlerBattery)
-    callipe = Callipe(car=car)
-    print(callipe.needs_service())
+    callipe = CarFactory.create_calliope(current_date=sprindlerBattery.current_date, \
+                                            last_service_date=sprindlerBattery.last_service_date, \
+                                            current_mileage=capuletEngine.current_mileage, \
+                                            last_service_mileage=capuletEngine.last_service_mileage)
+
+    print(callipe.engine.needs_service() or callipe.battery.needs_service())
